e0d9f4cdad51f3022005bf53682c33473117e893
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-01-21 16:35:10 -0300

    Add search posts by tags

--- includes/sidebar_widgets.php
+++ includes/sidebar_widgets.php
@@ -4,24 +4,28 @@ include 'db.php';
 <div class="col-md-4">
 
     <!-- Blog Search Well -->
+
     <div class="well">
-        <h4>Blog Search</h4>
-        <div class="input-group">
-            <input type="text" class="form-control">
-            <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
-                    <span class="glyphicon glyphicon-search"></span>
-                </button>
-            </span>
-        </div>
+        <h4>Search</h4>
+        <form action="search.php" method="post">
+
+            <div class="input-group">
+                <input name="searchQuery" type="text" class="form-control">
+                <span class="input-group-btn">
+                    <button name="submit" class="btn btn-default" type="submit">
+                        <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>
+            </div>
+        </form>
         <!-- /.input-group -->
     </div>
 
     <!-- Blog Categories Well -->
     <div class="well">
-        <h4>Blog Categories</h4>
+        <h4>Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
                     <?php
                     $categoryQuery = 'SELECT * FROM categories';
@@ -33,30 +37,10 @@ include 'db.php';
                         echo "<li><a href='#'>{$categoryName}</a>";
                     }
                     ?>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
+
         </div>
         <!-- /.row -->
     </div>
---
+++ search.php
@@ -0,0 +1,102 @@
+<?php
+include 'includes/header.php';
+?>
+<?php
+include 'includes/db.php';
+?>
+
+<!-- Navigation -->
+<?php
+include 'includes/navbar.php';
+?>
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+            <h1 class="page-header">
+                Page Heading
+                <small>Secondary Text</small>
+            </h1>
+
+            <!-- First Blog Post -->
+            <?php
+            if (isset($_POST['submit'])) {
+
+                // Sanitize inputs
+                $searchData = $_POST['searchQuery'];
+                $searchData = mysqli_real_escape_string($dbConnection, $searchData);
+
+                $tagQuery = "SELECT * FROM posts WHERE tags LIKE '%$searchData%'";
+
+                $tagQueryResults = mysqli_query($dbConnection, $tagQuery);
+
+                if (!$tagQueryResults) die('Error' . mysqli_error($dbConnection));
+
+                if (mysqli_num_rows($tagQueryResults) == 0) {
+                    echo '<h1> No posts found </h1>';
+                } else {
+                    echo 'found a post';
+                    while ($dbRow = mysqli_fetch_assoc($tagQueryResults)) {
+                        $postTags = $dbRow['tags'];
+                        $postAuthor = $dbRow['author'];
+                        $postTitle = $dbRow['title'];
+                        $postDate = $dbRow['date'];
+                        $postContent = $dbRow['content'];
+                        $postImage = $dbRow['image'];
+                        $postCommentAmount = $dbRow['comment_amount'];
+            ?>
+                        <h2>
+                            <a href="#"><?php echo $postTitle ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="index.php"> <?php echo $postAuthor ?> </a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $postDate ?></p>
+                        <hr>
+                        <img class="img-responsive" src="images/<?php echo $dbRow['image']; ?>" alt="">
+                        <hr>
+                        <p><?php echo $postContent ?></p>
+                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <?php
+                    }
+                }
+            }
+            ?>
+
+
+
+            <hr>
+
+
+
+        </div>
+
+        <!-- Blog Sidebar Widgets Column -->
+        <?php
+        include 'includes/sidebar_widgets.php';
+        ?>
+    </div>
+    <!-- /.row -->
+
+    <hr>
+    <!-- Footer -->
+    <?php
+    include 'includes/footer.php';
+    ?>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
