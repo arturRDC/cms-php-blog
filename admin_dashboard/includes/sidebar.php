841783d847f74d210736b3782e0c4001cd9f15ff
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-01-22 10:46:37 -0300

    Add starter administrator dashboard side menu

--- admin_dashboard/includes/sidebar.php
+++ admin_dashboard/includes/sidebar.php
@@ -4,19 +4,18 @@
             <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
         <li>
-            <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+            <a href="javascript:;" data-toggle="collapse" data-target="#posts-menu"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="posts-menu" class="collapse">
+                <li>
+                    <a href="#">Add new post</a>
+                </li>
+                <li>
+                    <a href="#">View all posts</a>
+                </li>
+            </ul>
         </li>
         <li>
-            <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
-        </li>
-        <li>
-            <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
-        </li>
-        <li>
-            <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-        </li>
-        <li>
-            <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
+            <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
         </li>
         <li>
             <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
@@ -29,11 +28,22 @@
                 </li>
             </ul>
         </li>
+        <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#users-menu"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+            <ul id="users-menu" class="collapse">
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+            </ul>
+        </li>
         <li class="active">
-            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
         </li>
         <li>
-            <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+            <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
         </li>
     </ul>
 </div>
