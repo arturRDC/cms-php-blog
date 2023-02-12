a3d9727fc8ab0bcb62a839d2860ed8b7917034ea
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-02-12 20:41:19 -0300

    Display username on admin dashboard

--- admin_dashboard/includes/navigation.php
+++ admin_dashboard/includes/navigation.php
@@ -1,3 +1,6 @@
+<?php
+$username = $_SESSION['username'];
+?>
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -13,7 +16,7 @@
     <ul class="nav navbar-right top-nav">
         <li><a href="../index.php">Home</a></li>
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $username ?> <b class="caret"></b></a>
             <ul class="dropdown-menu">
                 <li>
                     <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
