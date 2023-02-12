42b4b08fcfe34694c27cbbfaef4c210374a210e2
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-02-12 20:27:25 -0300

    Remove sidebar clutter

--- admin_dashboard/includes/sidebar.php
+++ admin_dashboard/includes/sidebar.php
@@ -18,17 +18,6 @@
             <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
         </li>
         <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="demo" class="collapse">
-                <li>
-                    <a href="#">Dropdown Item</a>
-                </li>
-                <li>
-                    <a href="#">Dropdown Item</a>
-                </li>
-            </ul>
-        </li>
-        <li>
             <a href="javascript:;" data-toggle="collapse" data-target="#users-menu"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
             <ul id="users-menu" class="collapse">
                 <li>
